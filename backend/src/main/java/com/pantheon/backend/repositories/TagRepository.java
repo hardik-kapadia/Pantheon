e6afb534cd5cec297bb4aaf93fd8c2f05eaf7b05
Hardik K <thecoderenroute@gmail.com>
2025-12-07 05:43:54 -0500

    fixing minor error

--- backend/src/main/java/com/pantheon/backend/repositories/TagRepository.java
+++ backend/src/main/java/com/pantheon/backend/repositories/TagRepository.java
@@ -1,10 +1,11 @@
 package com.pantheon.backend.repositories;
 
 import com.pantheon.backend.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface TagRepository {
+public interface TagRepository extends JpaRepository<Tag, Integer> {
 
     Optional<Tag> findByName(String name);
 
