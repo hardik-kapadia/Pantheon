45ba38dfd7bf934ab2d6f9ad75742045ff1a26c2
Hardik K <thecoderenroute@gmail.com>
2025-12-13 19:00:52 -0500

    fix(backend): Optimize config injections

--- backend/src/main/java/com/pantheon/backend/config/PantheonBackendConfig.java
+++ backend/src/main/java/com/pantheon/backend/config/PantheonBackendConfig.java
@@ -3,8 +3,10 @@ package com.pantheon.backend.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource(value = "classpath:app.cfg")
 public class PantheonBackendConfig {
 
     @Value("${app.sse.batch.size:100}")
