ee8293ee5cd5f8e933a0c9e465dfde850a6c892b
Hardik K <thecoderenroute@gmail.com>
2025-12-25 02:54:25 -0500

    Add electron components

---
+++ frontend/src/electron/main.ts
@@ -0,0 +1,22 @@
+import {app, BrowserWindow} from 'electron';
+import path from "node:path";
+import {isDev} from "./utils.js";
+
+app.on("ready", () => {
+    const mainWindow = new BrowserWindow({
+        webPreferences: {
+            preload: path.join(
+                app.getAppPath(),
+                '.',
+                "/dist-electron/preload.cjs"
+            )
+        }
+    });
+
+    if (isDev()) {
+        mainWindow.loadURL("http://localhost:5123");
+    } else {
+        mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+    }
+
+})
